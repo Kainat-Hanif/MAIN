0ffafc8bda44abafcd59941c150e2bd07a2cc9af
Kainat Hanif <sp24-bse-005@students.cuisahiwal.edu.pk>
2024-11-24 07:44:26 +0500

    Activity_1.java

--- Lab3/Activity_1.java
+++ Lab3/Activity_1.java
@@ -1,10 +1,11 @@
-// This peogram illustrates the use E notation.
+// This peogram illustrates the use of E notation.
 //Activity-1
 public class Activity_1 {
     public static void main(String[] args){
-        double distance, mass;
-        distance = 1.495979E11;
+        double distance, mass;  //Declaring
+        distance = 1.495979E11;  
         mass = 1.989E30;
+    //Results
         System.out.println("The sun is " + distance +" meters away.");
         System.out.println("The sun's mass is " + mass + " kilograms.");
         }
